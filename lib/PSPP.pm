5b8b9110a4ab0405d3efc590452ab907bb7ffc3b
Patrick Donelan <pat@patspam.com>
2009-07-28 21:02:02 +1000

    Improved tests, added POD

--- lib/PSPP.pm
+++ lib/PSPP.pm
@@ -6,6 +6,7 @@ use strict;
 use IPC::Run qw( run timeout );
 use Text::CSV_XS;
 use File::Temp;
+use Cwd;
 use Carp;
 
 =head1 NAME
@@ -20,8 +21,7 @@ PSPP is particularly aimed at statisticians, social scientists and students requ
 
 For more information, see L<http://www.gnu.org/software/pspp/>
 
-L<http://www.columbia.edu/acis/eds/stat_pak/spss/spss-write.html>
-L<http://www.ats.ucla.edu/stat/Spss/modules/input.htm>
+This module currently only contains one useful method, L<save>, which makes it easy to generate PSPP/SPSS-compatible .sav files.
 
 =head1 VERSION
 
@@ -33,9 +33,52 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
+    # Generate a SPSS-compatible .sav file from an array of data
+    my $pspp     = PSPP->new( verbose => 0 );
+    my $rows     = [
+        [ "AMC Concord",   22, 2930, 4099 ],
+        [ "AMC Pacer",     17, 3350, 4749 ],
+        [ "AMC Spirit",    22, 2640, 3799 ],
+        [ "Buick Century", 20, 3250, 4816 ],
+        [ "Buick Electra", 15, 4080, 7827 ],
+    ];
+    $pspp->save(
+        variables => 'make (A15) mpg weight price',
+        rows      => $rows,
+        outfile   => $outfile1,
+    ) or warn "An error occurred";
+
+    # Generate a csv file ourselves from $rows
+    my $csv = Text::CSV_XS->new( { binary => 1 } );
+    my $fh = File::Temp->new( SUFFIX => '.csv' );
+    for my $row (@$rows) {
+        $csv->print( $fh, $row );
+        print $fh "\n";
+    }
+    $fh->close;
+    $pspp->save(
+        variables => 'make (A15) mpg weight price',
+        infile    => $fh->filename,
+        outfile   => $outfile2,
+    ) or warn "An error occurred";
+
+=head1 methods
+
 =head2 new
 
 Constructor.
+
+=head3 Options
+
+=head4 verbose
+
+=head4 timeout
+
+The L<IPC::Run::run> timeout value
+
+=head4 pspp_binary
+
+The location of the C<pspp> binary. Defaults to C<pspp>.
 
 =cut
 
@@ -49,6 +92,32 @@ sub new {
         @_,
     }, $class;
 }
+
+=head2 save
+
+Generate a PSPP (and hence SPSS) compatible .sav file
+
+You must specify either C<rows> or C<infile>.
+
+=head3 Options
+
+=head4 outfile
+
+The name of the file to generate (defaults to out.sav)
+
+=head4 variables
+
+The PSPP/SPSS variables definition
+
+=head4 rows
+
+An array reference of rows to include in the data (optional)
+
+=head4 infile
+
+A data-file to read from (optional)
+
+=cut
 
 sub save {
     my $self      = shift;
@@ -85,46 +154,34 @@ END_SYNTAX
 
     print "Syntax:\n$syntax\n\n" if $self->verbose;
 
+    # Run in a temp dir so that pspp-generated files don't clash
+    my $cwd = cwd();
+    my $tmpdir = File::Temp->newdir();
+    chdir $tmpdir;
+    
     # Use IPC::Run to call PSPP binary
     my $pspp_binary = $self->{pspp_binary};
     run [$pspp_binary], \$syntax, \( my $out ), \( my $err ), timeout( $self->{timeout} )
         or croak "$pspp_binary: $?";
     carp $err if $err;
     print "Output:\n$out\n" if $self->verbose;
-    undef $infile;
+    
+    # Clean up
+    chdir $cwd;
     return -e $outfile;
 }
 
+=head2 verbose
+
+Returns true if the verbose flag is set
+
+=cut
+
 sub verbose { return $_[0]->{verbose} }
-
-########## TEST #################
-my $outfile1 = 'test1.sav';
-my $outfile2 = 'test2.sav';
-unlink $outfile1;
-unlink $outfile2;
-my $pspp = PSPP->new( verbose => 0 );
-$pspp->save(
-    variables => 'make (A15) mpg weight price',
-    rows      => [
-        [ "AMC Concord",   22, 2930, 4099 ],
-        [ "AMC Pacer",     17, 3350, 4749 ],
-        [ "AMC Spirit",    22, 2640, 3799 ],
-        [ "Buick Century", 20, 3250, 4816 ],
-        [ "Buick Electra", 15, 4080, 7827 ],
-    ],
-    outfile => $outfile1,
-) or warn "An error occurred";
-
-$pspp->save(
-    variables => 'make (A15) mpg weight price',
-    infile    => 'test.csv',
-    outfile   => $outfile2,
-) or warn "An error occurred";
-print "Finished\n";
 
 =head1 AUTHOR
 
-Patrick Donelan, C<< <pat at patspam.com> >>
+Patrick Donelan, C<< <pdonelan at cpan.org> >>
 
 =head1 BUGS
 
